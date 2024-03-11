f0c63d809fe9bc68d90758add576464d78ffecd0
linda <charming.linda85@gmail.com>
2024-03-11 17:48:24 +0300

    Внесена поправка в задачу 2.

--- src/Main.java
+++ src/Main.java
@@ -30,8 +30,8 @@ public class Main {
                 minPayment = payments2[i];
             }
         }
-        System.out.println("Минимальная сумма трат за неделю составила " + minPayment +
-                " рублей. Максимальная сумма трат за неделю составила " + maxPayment + " рублей");
+        System.out.println("Минимальная сумма трат за месяц составила " + minPayment +
+                " рублей. Максимальная сумма трат за месяц составила " + maxPayment + " рублей");
 
         System.out.println("Задача 3.");
         int[] payments3 = {174_000, 191_000, 202_500, 216_700, 146_200};
